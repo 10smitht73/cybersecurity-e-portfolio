37f779c31e823d38a50d6c2d973d0a6694bf5993
10smitht73 <31278117+10smitht73@users.noreply.github.com>
2024-05-28 15:08:54 +0000

    New SARM page with collapsible sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,21 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+document.addEventListener('DOMContentLoaded', function () {
+    var coll = document.getElementsByClassName("collapsible");
+    for (var i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+});
+
 (function($) {
 
 	var	$window = $(window),
